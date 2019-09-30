fe3d9d3d62ab71fefe9c55cb24c77d84deadb504
sudesh_ratch <sudesratch@gmail.com>
2019-09-30 12:19:59 +0700

    commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('main')
+
+@section('title','home')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>To do list App</h1>
+        <p>Aplikasi mencatat pekerjaan yang akan di kerjakan</p>
+    </div>
+    @endsection
---
+++ resources/views/list.blade.php
@@ -0,0 +1,7 @@
+@extends('main')
+
+@section('title','list')
+
+@section('content')
+        <h1>List to do</h1>
+    @endsection
